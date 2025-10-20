e6c98f5f8c70182f095e28750d81481f72db527a
Jonathan Teel <jonathan@jonathanteel.com>
2025-10-19 23:12:28 -0500

    db add in

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 "use client";
 
-const fallbackApiUrl = "http://161.35.62.220:9000/api";
+const fallbackApiUrl = 'https://deeezn.solfight.fun/api'
 
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_URL?.replace(/\/+$/, "") ?? fallbackApiUrl;
