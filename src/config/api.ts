1f5c923f37a2fcaebdda297ea5fc5c66f584ca49
Jonathan Teel <jonathan@jonathanteel.com>
2025-10-19 22:51:45 -0500

    mongo connect in

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 "use client";
 
-const fallbackApiUrl = "http://localhost:9000/api";
+const fallbackApiUrl = "http://161.35.62.220:9000/api";
 
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_URL?.replace(/\/+$/, "") ?? fallbackApiUrl;
